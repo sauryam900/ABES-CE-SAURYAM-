127c004d3cb0f9475f3ef5c155253391ca12fdd1
sauryam900 <sauryam.900@gmail.com>
2025-09-29 14:03:11 +0530

    first commit

--- Hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "SAURYAM" << endl << "SINGH"<< endl;
-    return 0;
-}
-
--- Sum.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
- int main(){
-    int n1,n2,n3,n4,res;
-    cin>>n1>>n2>>n3>>n4;
-    res=n1+n2+n3+n4;
-    cout<<res<<endl;
- }
